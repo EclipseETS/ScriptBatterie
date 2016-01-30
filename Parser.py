0793408c06450af077f6069286dfc668e3425029
Francis <francky502@hotmail.com>
2016-01-29 20:04:35 -0500

    Ajouté des commentaires

--- Parser.py
+++ Parser.py
@@ -1,11 +1,17 @@
-#Takes all data from result.txt and put it in a dataframe
+"""Ce script <<parse>> le fichier text contenant les résultats du cyclage des cellules 
+   pour les insérer dans un DataFrame et ensuite déterminer 38 groupes de 11 cellules"""
+   
 import pandas as pd
 import numpy as np
 import collections
 
+#Création du dataframe
 result=pd.DataFrame(np.nan, index=[], columns=['Salve','Cellule','InternalRes','Capacity'])
 
 file=open('result.txt','r')
+
+#Chaque ligne du fichier est transformé en liste contenant seulement les 
+#info nécessaire, puis la liste est ajoutée au dataframe
 for num, line in enumerate(file):
     list=line.split(":")
     list[0]=str(list[0])[-1]
@@ -17,8 +23,13 @@ file.close()
 
 #La variance représente le niveau d'écart des données avec la moyenne.
 #Pour optimiser les groupes de cellules, on cherche à avoir la plus petite variance de résistance interne
-
+#La variable final représente la performance MOYENNE de chaque cellule pour X salve de test
+#ex: résistance interne moyenne de la cellule 1 pour 9 salve
 final=result.groupby('Cellule')['InternalRes','Capacity'].mean()
+
+#Le dataframe est ordonné avec résistance interne déscendante,
+#puis les 418 cellules avec la meilleure capacité sont retenues
 final=final.sort_values('Capacity', ascending=False)
 final['index'] = range(1, len(final) + 1)
 final = final[final['index'] < 418]
+
